dca6d877640890445d29582d20b02dbff482600b
Cyanider <amihasan420@gmail.com>
2024-10-03 19:21:14 +0600

    day 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import {ModeToggle} from "@/components/Theme-Switcher";
+import { ModeToggle } from "@/components/Theme-Switcher";
 
 export default function Home() {
   return (
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex-col items-center max-w-3xl">
         {/* <ModeToggle /> */}
-        <h1>
+        <h1 className="text-4xl font-bold tracking-tighter text-gray-900 sm:text-6xl">
           Your marketplace for all high quality{" "}
-          <span className="text-blue-600">electronic assets</span>{" "}
+          <span className="text-blue-600">Electronic assets</span>.
         </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Welcomoe to Easy-Sell. Every asset on our platform is varified by our
+          team to ensure our hightest quality standards.
+        </p>
       </div>
     </MaxWidthWrapper>
   );
