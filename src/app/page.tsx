e92b411583f7300248cc54391055af2d9866446f
Cyanider <amihasan420@gmail.com>
2024-10-05 17:11:35 +0600

    day 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,42 @@ import { buttonVariants } from "@/components/ui/button";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+const perks = [
+  {
+    name: "Instant Delivery",
+  },
+];
+
 export default function Home() {
   return (
-    <MaxWidthWrapper>
-      <div className="py-20 mx-auto text-center flex-col items-center max-w-3xl">
-        {/* <ModeToggle /> */}
-        <h1 className="text-4xl font-bold tracking-tighter text-gray-900 sm:text-6xl">
-          Your marketplace for all high quality{" "}
-          <span className="text-blue-600">Electronic assets</span>.
-        </h1>
-        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
-          Welcomoe to Easy-Sell. Every asset on our platform is varified by our
-          team to ensure our hightest quality standards.
-        </p>
-        <div className="flex flex-col sm:flex-row gap-4 mt-6">
-          <Link href="/products" className={buttonVariants()}>
-            Browse Trending
-          </Link>
-          <Button variant="ghost">Our quality promise &rarr;</Button>
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex-col items-center max-w-3xl">
+          {/* <ModeToggle /> */}
+          <h1 className="text-4xl font-bold tracking-tighter text-gray-900 sm:text-6xl">
+            Your marketplace for all high quality{" "}
+            <span className="text-blue-600">Electronic assets</span>.
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Welcomoe to Easy-Sell. Every asset on our platform is varified by
+            our team to ensure our hightest quality standards.
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href="/products" className={buttonVariants()}>
+              Browse Trending
+            </Link>
+            <Button variant="ghost">Our quality promise &rarr;</Button>
+          </div>
         </div>
-      </div>
-    </MaxWidthWrapper>
+
+        {/* TODO: List Products */}
+      </MaxWidthWrapper>
+
+      <section className="border-t border-gray-200 bg-gray-50">
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0"></div>
+        </MaxWidthWrapper>
+      </section>
+    </>
   );
 }
