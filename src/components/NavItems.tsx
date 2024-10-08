85dadfd262e86b9b5fedd34c17d615f649b7a59b
Cyanider <amihasan420@gmail.com>
2024-10-08 20:10:29 +0600

    day 7

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,11 +1,28 @@
 "use client";
 
+import { PRODUCT_CATEGORIES } from "@/config";
 import React, { useState } from "react";
+import NavItem from "@/components/NavItem"
 
 const NavItems = () => {
-    const [activeIndex,setActiveIndex] = useState<null |number>(null)
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
-  return <div className="flex gap-4 h-full"></div>;
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+
+        const isOpen = i === activeIndex;
+        return <NavItem />;
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,38 @@ export const PRODUCT_CATEGORIES = [
       {
         name: "Editor Pics",
         href: "#",
-        imageSrc: "",
+        imageSrc: "/nav/ui_kits/mixed.jpeg",
+      },
+      {
+        name: "New Arrival",
+        href: "#",
+        imageSrc: "/nav/ui_kits/blue.jpeg",
+      },
+      {
+        name: "Best-Seller",
+        href: "#",
+        imageSrc: "/nav/ui_kits/purple.jpeg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favourite Icon pics",
+        href: "#",
+        imageSrc: "/nav/icon/picks.jpeg",
+      },
+      {
+        name: "New Arrival",
+        href: "#",
+        imageSrc: "/nav/icons/new.jpeg",
+      },
+      {
+        name: "Best Selling Products",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.jpeg",
       },
     ],
   },
